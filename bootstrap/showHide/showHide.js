2170c06450b3e9738bd1a20749281f69aa92f874
Nutan-Gyawali <nutangyawali01@gmail.com>
2024-05-11 21:25:38 +0545

    created a show hide button

---
+++ bootstrap/showHide/showHide.js
@@ -0,0 +1,20 @@
+function hide()
+{
+   var button = document.getElementById("hide");
+   console.log(button);
+
+
+    console.log(button.innerHTML);
+    var inn = button.innerHTML;
+
+    if(inn == "Hide It")
+    button.innerHTML = 'Show it';
+    
+    else
+    button.innerHTML = 'Hide It';
+   
+
+   console.log(button);
+
+   
+}
